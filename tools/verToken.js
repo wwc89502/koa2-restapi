9ec01bdee53c8ff57efdbd72a5b77685996e5396
Wang Weichen <wwc89502@gmail.com>
2019-10-28 10:36:18 +0800

    修改获取用户信息为根据token获取

--- tools/verToken.js
+++ tools/verToken.js
@@ -3,9 +3,7 @@ import jwt from 'jsonwebtoken'
 const verToken = token => {
   return new Promise((resolve, rejece) => {
     const info = jwt.verify(token.split(' ')[1], global.koajwtStr)
-    resolve({
-      status: 1
-    })
+    resolve(info)
     rejece({
       status: 0,
       message : info.message
